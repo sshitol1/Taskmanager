94bb77ec1ab999d0bace04afb2f174cfbbdee73f
Soham Shitole <sshitole1@binghamton.edu>
2024-03-15 08:43:54 -0400

    Implemented aws cognito with javascript

---
+++ aws-exports.js
@@ -0,0 +1,10 @@
+const awsConfig = {
+    Auth: {
+        region: 'us-east-1', // Your AWS region
+        userPoolId: 'us-east-1_bwtj0CGIu', // Your Cognito User Pool ID
+        userPoolWebClientId: '4j8rkvo4ltu87m9i916fcuvjdd', // Your Cognito App Client ID
+    }
+    // Include other services here
+};
+
+export default awsConfig;
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+// app.js or index.js
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+
+Amplify.configure(awsConfig);
+
+// Your code to handle user authentication, routing, etc.
